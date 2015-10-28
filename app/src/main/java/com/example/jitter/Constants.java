afda84545a33001136519e05ff374c05e9cc1284
Atabek <atabekmur@gmail.com>
2015-10-29 09:46:32 +1100

    Implement basic validation for twitter usernames in MainActivity

--- app/src/main/java/com/example/jitter/Constants.java
+++ app/src/main/java/com/example/jitter/Constants.java
@@ -2,4 +2,10 @@ package com.example.jitter;
 
 public class Constants {
     public static final String TWITTER_USER_NAME = "twitter_user_name";
+    public static String TWITTER_USER_TIMELINE_API =
+            "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=";
+    public static String TWITTER_FAVORITES_LIST_API =
+            "https://api.twitter.com/1.1/favorites/list.json?screen_name=";
+    public static String TWITTER_USERNAME_PATTERN = "([A-Za-z]|[0-9]|\\_)+";
+    public static int TWITTER_USERNAME_SIZE = 15;
 }
--- app/src/main/java/com/example/jitter/MainActivity.java
+++ app/src/main/java/com/example/jitter/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText editTextTwitterName = (EditText) findViewById(R.id.edit_text_twitter_id);
+        final EditText etTwitterName = (EditText) findViewById(R.id.edit_text_twitter_id);
+        final TextView tvTwitterNameError = (TextView) findViewById(R.id.text_view_twitter_id_error);
         Button buttonSearch = (Button) findViewById(R.id.button_search);
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editTextTwitterName.length() > 0) {
+                String twitterName = etTwitterName.getText().toString();
+                tvTwitterNameError.setVisibility(View.GONE);
+                if (twitterName.length() > 1 &&
+                        twitterName.length() <= Constants.TWITTER_USERNAME_SIZE &&
+                        twitterName.matches(Constants.TWITTER_USERNAME_PATTERN)) {
                     Intent intent = new Intent(MainActivity.this, UsersActivity.class);
-                    intent.putExtra(Constants.TWITTER_USER_NAME, editTextTwitterName.getText().toString());
+                    intent.putExtra(Constants.TWITTER_USER_NAME, twitterName);
                     startActivity(intent);
+                } else {
+                    etTwitterName.setText("");
+                    tvTwitterNameError.setVisibility(View.VISIBLE);
                 }
             }
         });
